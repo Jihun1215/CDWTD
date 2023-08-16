541110a751f6cb06a206ffab2d74b163746284d9
음지훈 <jhmc5268@naver.com>
2023-08-16 17:31:27 +0900

    Feat: 초기 세팅 완료

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,20 @@
 import React from 'react';
+import { ThemeProvider } from 'styled-components';
+import { GlobalStyle } from './styles/Globalstyle';
+import { Theme } from './styles/theme';
+import Reset from './styles/Reset';
 
 function App() {
+
+  const theme = {...Theme}
+
   return (
-    <div>2
-    </div>
+    <ThemeProvider theme={theme}>
+    <Reset />
+    <GlobalStyle />
+    2
+        {/* <Router/> */}
+    </ThemeProvider>
   );
 }
 
