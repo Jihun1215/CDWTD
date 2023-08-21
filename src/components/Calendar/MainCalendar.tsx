f1bf2f28866189a0ce27c1f0b7caf57bfbf9c547
음지훈 <jhmc5268@naver.com>
2023-08-21 12:56:10 +0900

    Feat: 중간 컴포넌트 삭제

--- src/components/Calendar/MainCalendar.tsx
+++ src/components/Calendar/MainCalendar.tsx
@@ -1,12 +1,22 @@
 import React from 'react'
 import { styled } from 'styled-components'
-import DateBox from './DateBox'
+// import DateBox from './DateBox'
+import { useRecoilState } from 'recoil';
+import { WeeksState } from '../../atoms'
+import WeekBox from './WeekBox';
 
 const MainCalendar:React.FC= () => {
-
+  const [weeks] = useRecoilState(WeeksState)
   return (
     <MainCalendarContiner>
-      <DateBox />
+      <DateBoxContainer>
+        
+        {weeks.map((week:string)=>{
+          return <WeekBox weekName={week}></WeekBox>
+         })}
+
+
+      </DateBoxContainer>
     </MainCalendarContiner>
   )
 }
@@ -17,3 +27,11 @@ const MainCalendarContiner = styled.div`
     width: 100%;
     height: 90%;
 `;
+
+const DateBoxContainer = styled.div`
+  width: 100%;
+  flex: 1;
+  display: grid;
+  grid-template-columns: repeat(7, 1fr);
+`;
+
